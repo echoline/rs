2453201e0db5c4def9dd0ee29ec0221e3b1433eb
eli <echoline@gmail.com>
2016-09-01 14:04:00 -0700

    english markov

--- make_alice_text.sh
+++ make_alice_text.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
 FNAME="la_aventuroj_de_alicio_en_mirlando.txt"
-
-LEN=`wc -l $FNAME | cut -f 1 -d ' '`
+LEN=4352
 TAIL=`expr $LEN - 242`
 HEAD=`expr $TAIL - 425`
 
-tail -n $TAIL $FNAME | head -n $HEAD | sed 's/$/ /g' | tr -s ' ' | tr -d '\n'
+if test `wc -l $FNAME | cut -f 1 -d ' '` -eq $LEN; then
+  tail -n $TAIL $FNAME | head -n $HEAD | sed 's/$/ /g' | tr -d '\n' | tr -s ' '
+fi
+
+FNAME="alices_adventures_in_wonderland.txt"
+LEN=3735
+TAIL=`expr $LEN - 39`
+HEAD=`expr $TAIL - 365`
+
+if test `wc -l $FNAME | cut -f 1 -d ' '` -eq $LEN; then
+  tail -n $TAIL $FNAME | head -n $HEAD | sed 's/$/ /g' | tr -d '\n' | tr -s ' '
+fi
